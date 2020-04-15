f4b456d6f7a772ff18d91c4a99aaad7037cf685b
Claudia Valdivieso <claumavaldivieso@gmail.com>
2020-04-15 16:49:31 -0500

    Updates

---
+++ precache-manifest.f059306c4526bab1c7c10bf3ea5e0f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b9c442858a3aa35939d91d7cb7c6d0",
+    "url": "/demos/slate-editor-example/index.html"
+  },
+  {
+    "revision": "84f9c96ccc55ba5b3642",
+    "url": "/demos/slate-editor-example/static/css/main.c1c25d9f.chunk.css"
+  },
+  {
+    "revision": "787f9261ab28520ad425",
+    "url": "/demos/slate-editor-example/static/js/2.2e45f874.chunk.js"
+  },
+  {
+    "revision": "ebe3e3146ec8811972f3f01e6a145274",
+    "url": "/demos/slate-editor-example/static/js/2.2e45f874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f9c96ccc55ba5b3642",
+    "url": "/demos/slate-editor-example/static/js/main.a2235f0c.chunk.js"
+  },
+  {
+    "revision": "4d055cf41585a7700795",
+    "url": "/demos/slate-editor-example/static/js/runtime-main.ef984aaf.js"
+  }
+]);
