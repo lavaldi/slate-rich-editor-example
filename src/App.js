8ef84cbcb10691e5cb04d1fde9c94d50a2fc83aa
Claudia Valdivieso <claumavaldivieso@gmail.com>
2020-04-15 21:33:05 -0500

    serialize editor value to HTML

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './styles.css';
 
 const editorValue = '<ul class="md-block-unordered-list-item"><li>It\'s rare that TLs are invited to participate in the pitch review process.</li></ul>'
 
+// const editorValue = '<ul><li>It\'s rare that TLs are invited to participate in the pitch <strong>review</strong> process.</li><img alt="" src="https://p-VVF5MJM.t2.n0.cdn.getcloudapp.com/items/2NuXk4j8/Image%202020-04-14%20at%202.39.07%20PM.png?v=1f4b0ec164546d136b3f61a1f38c9aef" /><p></p></ul>'
+
 function App() {
   return (
     <section className="app">
